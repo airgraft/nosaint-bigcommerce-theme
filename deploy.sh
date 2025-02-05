9accb9a8fef5ffe8888741c2c635d4ff74fe0cf1
Laurent Laporte <laur3nt.lapo@gmail.com>
2025-02-05 16:18:28 -0500

    Update

--- deploy.sh
+++ deploy.sh
@@ -17,8 +17,10 @@ done
 sed -i '' "s/\"version\": \".*\"/\"version\": \"$CURRENT_DATETIME\"/" config.json
 
 # Define arrays for account configurations
-ACCESS_TOKENS=("6movuboeofd1ltpz37t471rid5ubolt" "k11ia15u54bl60el1mfyrft22yacn4e")
-STORE_URLS=("https://nosaint.mybigcommerce.com" "https://nosaint-ca.mybigcommerce.com")
+# ACCESS_TOKENS=("6movuboeofd1ltpz37t471rid5ubolt" "k11ia15u54bl60el1mfyrft22yacn4e")
+# STORE_URLS=("https://nosaint.mybigcommerce.com" "https://nosaint-ca.mybigcommerce.com")
+ACCESS_TOKENS=("6movuboeofd1ltpz37t471rid5ubolt")
+STORE_URLS=("https://nosaint.mybigcommerce.com")
 
 # Loop through each account and perform the stencil push
 for i in "${!ACCESS_TOKENS[@]}"; do
